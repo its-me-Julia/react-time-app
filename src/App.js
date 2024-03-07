3d4de3a8de9dbb8c3d4b824f01d5100cd117a94d
its-me-Julia <142026643+its-me-Julia@users.noreply.github.com>
2024-03-07 16:44:15 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        <p class="footer-text">
+          - Coded by{" "}
+          <a class="footer-link" href="https://github.com/its-me-Julia">
+            JuliaD
+          </a>
+          -
+        </p>
+      </footer>
+    </div>
+  );
 }
